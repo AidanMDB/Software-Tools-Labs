e391a9406338c1fdc4824752c308e608f8cda681
Student A10 EE364 <ee364a10@eceprog2.ecn.purdue.edu>
2024-01-16 13:48:36 -0500

    finished part 1

--- Lab01/Lab01Module.py
+++ Lab01/Lab01Module.py
@@ -1,8 +1,8 @@
 # ######################################################
-# Author : 
-# email : @purdue.edu
-# ID : ee364XXX
-# Date : 
+# Author : Aidan Dannhausen-Brun
+# email : adannhau@purdue.edu
+# ID : ee364a10
+# Date : 1/16/24
 # ######################################################
 import os # List of module import statements
 import sys # Each one on a line
@@ -18,7 +18,21 @@ def oddResult(number_list, string):
     If the string is ”product”, return the product of all the elements that are odd in the list."""
 
     # DO SOMETHING HERE
+    odd_list = []
+
+    for i in number_list:
+        if i % 2 == 1:
+            odd_list.append(i)
+
     result = 0
+
+    if string == "sum":
+        result = sum(odd_list)
+    else:
+        result = odd_list[0]
+        for i in range(1, len(odd_list)):
+            result *= odd_list[i]
+
     return result
         
 # This block is optional and can be used for testing .
@@ -26,7 +40,7 @@ def oddResult(number_list, string):
 # ######################################################
 if __name__ == "__main__":
     number_list = list(range(1,6))  # [1, 2, 3, 4, 5]
-    result = oddResult(number_list, "product")
+    result = oddResult(number_list, "sum")
     print(result)
     
 
