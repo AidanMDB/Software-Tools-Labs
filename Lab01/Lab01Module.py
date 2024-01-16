489c315e13e9f5ea888e2a218d9c64c271bd587e
Student A10 EE364 <ee364a10@eceprog3.ecn.purdue.edu>
2024-01-16 13:35:07 -0500

    lab 01 base commit

---
+++ Lab01/Lab01Module.py
@@ -0,0 +1,32 @@
+# ######################################################
+# Author : 
+# email : @purdue.edu
+# ID : ee364XXX
+# Date : 
+# ######################################################
+import os # List of module import statements
+import sys # Each one on a line
+# ######################################################
+# No Module - Level Variables or Statements !
+# ONLY FUNCTIONS BEYOND THIS POINT !
+# ######################################################
+
+def oddResult(number_list, string):
+    """Odd Number Result
+    
+    If the string is ”sum”, return the sum of all the elements that are odd in the list. 
+    If the string is ”product”, return the product of all the elements that are odd in the list."""
+
+    # DO SOMETHING HERE
+    result = 0
+    return result
+        
+# This block is optional and can be used for testing .
+# We will NOT look into its content .
+# ######################################################
+if __name__ == "__main__":
+    number_list = list(range(1,6))  # [1, 2, 3, 4, 5]
+    result = oddResult(number_list, "product")
+    print(result)
+    
+
